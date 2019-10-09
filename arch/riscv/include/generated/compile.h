ed7d0c28ac80b16b266bf48036d1afd48e99ad69
Janei <1021873147@qq.com>
2019-10-10 00:49:22 +0800

    complete a fail head

--- arch/riscv/include/generated/compile.h
+++
@@ -1,7 +0,0 @@
-/* This file is auto generated, version 17 */
-/* SMP */
-#define UTS_MACHINE "riscv64"
-#define UTS_VERSION "#17 SMP Sat Sep 28 10:08:53 CST 2019"
-#define LINUX_COMPILE_BY "janei"
-#define LINUX_COMPILE_HOST "Janei"
-#define LINUX_COMPILER "gcc version 9.2.0 (GCC)"
