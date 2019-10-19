964645a2bfad7df84682380973b8d17081e075e7
Janei <1021873147@qq.com>
2019-10-19 16:04:44 +0800

    enable time interrupt

--- lab2/arch/riscv/kernel/mtrap.c
+++ lab2/arch/riscv/kernel/mtrap.c
@@ -1,7 +1,13 @@
 #include <ns16550a.h>
 
+// uint8_t *clint = (uint8_t *)(void *)SIFIVE_CLINT_CTRL_ADDR;
+int count = 0;
+
 void trap_handler() 
 {
-    ns16550a_init();
-    ns16550a_putchar('h');
+   ns16550a_init();
+   ns16550a_putchar('0' + count++);
+   ns16550a_putchar('\n');
+   if(count > 9)
+      count  = 0;
 }
--- lab2/init/main.c
+++ lab2/init/main.c
@@ -2,11 +2,5 @@
 
 void start_kernel()
 {
-    char str[20] = "Hello RISC-V! "; 
-    
-    ns16550a_init();
-    for (int i = 0; str[i] != '\0'; i++)
-    {
-        ns16550a_putchar(str[i]);
-    }
+    while(1) {;}
 }
